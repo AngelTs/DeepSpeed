7f471e4a1768b5e34dbcf61abf85518601993ce5
Jeff Rasley <jerasley@microsoft.com>
2022-09-07 16:25:34 -0700

    updates to bench scripts

--- benchmarks/inference/run_model.sh
+++ benchmarks/inference/run_model.sh
@@ -1,23 +1,22 @@
 set -ex
 
-model="EleutherAI/gpt-neo-2.7B"
+model=$1
 
 version=0
 log_path=results/${model}_v${version}
 mkdir -p ${log_path}
 
-echo "baseline"
+echo "baseline $log_path"
 deepspeed --num_gpus 1 gpt-bench.py -m "${model}" &> ${log_path}/base.log
 
 cd ../../
 git checkout staging-ds-azure-v1
 cd -
-echo "ds azure"
+echo "ds azure $log_path"
 deepspeed --num_gpus 1 gpt-bench.py --deepspeed -m "${model}" &> ${log_path}/dsAzure.log
 
 cd ../../
 git checkout master
 cd -
-echo "ds public"
+echo "ds public $log_path"
 deepspeed --num_gpus 1 gpt-bench.py --deepspeed -m "${model}" &> ${log_path}/dsPublic.log
-
